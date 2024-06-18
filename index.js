76c9923d08505e6db3d5d311efcd23c5635ebc7c
WALUKA <128817870+Walukapahantt@users.noreply.github.com>
2024-06-18 15:28:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Your Live Stream Is Online Now!')
-})
+const port = 8080
 
 app.listen(port, () => {
   console.log(`Put The web Adress In Uptimerobot!`)
